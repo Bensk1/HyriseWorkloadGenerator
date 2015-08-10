ffd269b47d8ac47b74deb209d4b93a1b7ebf5474
Jan Kossmann <jan.kossmann@student.hpi.uni-potsdam.de>
2015-08-10 16:23:40 +0200

    Add threadpool for query execution. Extend QueryClass (wip)

--- queryClass.py
+++ queryClass.py
@@ -1,8 +1,26 @@
+import linecache
+
 class QueryClass:
 
-    def __init__(self, description):
+    def __init__(self, description, table, columns, values, tableDirectory):
         self.description = description
+        self.table = table
+        self.columns = columns
+        self.values = values
+        self.tableDirectory = tableDirectory
+        self.datatypes = []
+        self.queryJson = self.createQueryJson()
 
-    def execute(self):
-        # print self.description
-        pass
+    def createQueryJson(self):
+        self.determineDatatypes()
+
+    def determineDatatypes(self):
+        for column in self.columns:
+            tableFile = "%s/%s.tbl" % (self.tableDirectory, self.table)
+            datatypes = linecache.getline(tableFile, 2)
+            self.datatypes.append(datatypes.split('|')[column])
+
+    def execute(self, batches, concurrencyLevel):
+        # print requests,
+        print "%i %i" % (batches, concurrencyLevel)
+        # return concurrencyLevel
--- workload.py
+++ workload.py
@@ -2,11 +2,22 @@
 
 import sys
 import json
+import types
+import copy_reg
+from multiprocessing import Pool
 from queryClass import QueryClass
 from queryClassDistribution import QueryClassDistribution
 
 QUERY_DIVISOR = 100
 DISTRIBUTION_DIVISOR = 5
+
+def _pickle_method(m):
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
+
+copy_reg.pickle(types.MethodType, _pickle_method)
 
 class Object:
     def toJSON(self):
@@ -14,11 +25,11 @@ class Object:
 
 class Workload(Object):
 
-    def __init__(self, days, queriesPerDay, queryClasses, queryClassDistributions, verbose):
+    def __init__(self, days, queriesPerDay, queryClasses, queryClassDistributions, verbose, tableDirectory):
         self.days = days
         self.currentDay = 1
         self.queriesPerDay = queriesPerDay
-        self.queryClasses = self.parseQueryClasses(queryClasses)
+        self.queryClasses = self.parseQueryClasses(queryClasses, tableDirectory)
         self.queryClassDistributions = self.parseQueryClassDistributions(queryClassDistributions)
         self.verbose = verbose
         self.currentlyActiveQueryDistribution = None
@@ -26,12 +37,13 @@ class Workload(Object):
         self.activeQueryClassDistributionChanged = False
         self.currentQueryBatchOrder = None
         self.batches = self.queriesPerDay / (QUERY_DIVISOR / DISTRIBUTION_DIVISOR)
+        self.threadPool = Pool(len(self.queryClasses))
 
-    def parseQueryClasses(self, queryClasses):
+    def parseQueryClasses(self, queryClasses, tableDirectory):
         queryClassesParsed = []
 
         for queryClass in queryClasses:
-            queryClassesParsed.append(QueryClass(queryClass['description']))
+            queryClassesParsed.append(QueryClass(queryClass['description'], queryClass['table'], queryClass['columns'], queryClass['values'], tableDirectory))
 
         return queryClassesParsed
 
@@ -84,18 +96,26 @@ class Workload(Object):
                 for numberOfQueries, queryClass in zip(self.currentQueryBatchOrder, self.queryClasses):
                     print "%i queries of type %s" % (numberOfQueries, queryClass.description)
 
-            for query in self.currentQueryOrder:
-                query.execute()
+            self.threadPoolResults = []
 
+            for numberOfQueries, queryClass in zip(self.currentQueryBatchOrder, self.queryClasses):
+                self.threadPoolResults.append(self.threadPool.apply_async(queryClass.execute, [self.batches, numberOfQueries]))
+
+
+            for threadPoolResult in self.threadPoolResults:
+                threadPoolResult.wait()
 
             self.currentDay += 1
 
-if len(sys.argv) <> 2:
-    print "Usage: python generator.py workload.json"
+if len(sys.argv) <> 3:
+    print "Usage: python generator.py workload.json tableDirectory"
     sys.exit()
 else:
-    with open(sys.argv[1]) as workloadFile:
+    workloadConfigFile = sys.argv[1]
+    tableDirectory = sys.argv[2]
+
+    with open(workloadConfigFile) as workloadFile:
             workloadConfig = json.load(workloadFile)
 
-    w = Workload(workloadConfig['days'], workloadConfig['queriesPerDay'], workloadConfig['queryClasses'], workloadConfig['queryClassDistributions'], workloadConfig['verbose'])
+    w = Workload(workloadConfig['days'], workloadConfig['queriesPerDay'], workloadConfig['queryClasses'], workloadConfig['queryClassDistributions'], workloadConfig['verbose'], tableDirectory)
     w.run()
