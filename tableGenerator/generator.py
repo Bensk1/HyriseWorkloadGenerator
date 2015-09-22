a3b9509aacf4c99d8e8da377c0228391b72486f4
Jan Kossmann <jan.kossmann@student.hpi.uni-potsdam.de>
2015-09-22 11:19:56 +0200

    Improve last commit

--- tableGenerator/generator.py
+++ tableGenerator/generator.py
@@ -1,12 +1,16 @@
 import sys
 import json
 from table import Table
+from random import seed
 
 def buildTables(configFile, outputDirectory):
     with open(configFile) as configFile:
         tableConfigs = json.load(configFile)
 
     metaDataFile = open("%s/metadata" % (outputDirectory), "w")
+
+    # For testing purposes, uncomment for random tables
+    seed(1238585430324)
 
     for tableConfig in tableConfigs:
         table = Table(tableConfig['name'], tableConfig['rows'], tableConfig['columns'], tableConfig['stringsForEachInt'], tableConfig['stringLength'], tableConfig['uniqueValues'], outputDirectory, metaDataFile)
