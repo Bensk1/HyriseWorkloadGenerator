93fd44108381946e71f13ac6a8d3e75332e87e84
Jan Kossmann <jan.kossmann@student.hpi.uni-potsdam.de>
2015-12-29 14:29:21 +0100

    Add debugPrint to column

--- column.py
+++ column.py
@@ -10,3 +10,6 @@ class Column():
         self.predicateType = predicateType
         self.value = value
         self.datatype = DATATYPES[datatype]
+
+    def debugPrint(self):
+        print "Column %i predicateType %s value %s datatype %i" % (self.column, self.predicateType, self.value, self.datatype)
